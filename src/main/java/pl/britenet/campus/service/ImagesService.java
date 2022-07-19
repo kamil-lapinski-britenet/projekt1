613724bc3de11fb5a1883debaa2ab5d8872e51c6
kamil.lapinski <kamil.lapinski@britenet.com.pl>
2022-07-19 11:16:51 +0200

    update commit

--- src/main/java/pl/britenet/campus/service/ImagesService.java
+++ src/main/java/pl/britenet/campus/service/ImagesService.java
@@ -2,8 +2,15 @@ package pl.britenet.campus.service;
 
 import pl.britenet.campus.database.DatabaseService;
 import pl.britenet.campus.database.object.Images;
+import pl.britenet.campus.database.object.Order;
 import pl.britenet.campus.database.object.Product;
 import pl.britenet.campus.database.object.User;
+
+import java.awt.*;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 public class ImagesService {
 
@@ -28,6 +35,43 @@ public class ImagesService {
                 , images.getPaths(), images.getProductId()));
 
 
+    }
+
+    public List<Images> getImages() {
+        return this.databaseService.performSQL("SELECT * FROM images", resultSet -> {
+            List<Images> imagesList = new ArrayList<>();
+            try {
+                while (resultSet.next()) {
+                    Images images = new Images(resultSet.getInt("imageId"));
+                    images.setPaths(resultSet.getString("paths"));
+                    images.setUserId(resultSet.getInt("userId"));
+                    images.setProductId(resultSet.getInt("productId"));
+                    imagesList.add(images);
+                }
+            } catch (SQLException exception) {
+                throw new IllegalStateException(exception);
+            }
+            return imagesList;
+        });
+    }
+
+    public Optional<Images> getImage(int imageId) {
+        Images retrieveImage = this.databaseService.performSQL(String.format("SELECT * FROM images WHERE imageId = %d", imageId), resultSet -> {
+            try {
+                if (resultSet.next()) {
+                    Images images = new Images(resultSet.getInt("imageId"));
+                    images.setPaths(resultSet.getString("paths"));
+                    images.setUserId(resultSet.getInt("userId"));
+                    images.setProductId(resultSet.getInt("productId"));
+                    return images;
+                }
+            } catch (SQLException exception) {
+                throw new IllegalStateException(exception);
+            }
+            return null;
+        });
+
+        return Optional.of(retrieveImage);
     }
 
     public void deleteImage (int imageId) {
